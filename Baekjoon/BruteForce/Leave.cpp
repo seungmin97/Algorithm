517e4a6aabd084dff918c5f5338745e737b1a2de
seungmin97 <lsm0341@naver.com>
2019-09-17 10:58:24 +0900

    2019-09-17

--- Baekjoon/BruteForce/Leave.cpp
+++ Baekjoon/BruteForce/Leave.cpp
@@ -1,4 +1,43 @@
 //
-// Created by 이승민 on 2019-09-13.
+// Created by user on 2019-09-16.
 //
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define Max 15
+using namespace std;
+
+vector <pair<int, int>> v(Max);
+
+int max_;
+int N;
+void work(int i, int profit){
+
+
+    if((i + v[i].first) <= N){
+        profit += v[i].second;
+    }
+
+    max_ = max(profit, max_);
+
+    for (int j = i + v[i].first; j < N ; ++j) {
+        work(j, profit);
+    }
+}
+
+int main(){
+
+    cin >> N;
+
+    for (int i = 0; i < N; ++i) {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    for (int i = 0; i < N; ++i) {
+        work(i, 0);
+    }
+
+    cout << max_;
+    return 0;
+}
