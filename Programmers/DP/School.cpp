9e8966a9f23254c91493ff7a3334bac2f5f03f38
seungmin97 <lsm0341@naver.com>
2019-12-03 15:34:41 +0900

    2019-12-03

--- Programmers/DP/School.cpp
+++ Programmers/DP/School.cpp
@@ -7,8 +7,6 @@
 #include <vector>
 #include <string.h>
 using namespace std;
-//int map[101][101]={};
-//int check[101][101]={0,};
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
     int **map = new int*[n];
@@ -54,52 +52,3 @@ int main(){
 
     return 0;
 }
-
-/*
- * #include <string>
-#include <string.h>
-#include <vector>
-using namespace std;
-int solution(int m, int n, vector<vector<int>> puddles) {
-    int answer = 0;
-
-    int **count = new int*[n];
-
-    for (int i = 0; i < n; ++i) {
-        count[i] = new int[m];
-        memset(count[i], 0 ,sizeof(int) * (m));
-    }
-    for(int i=0;i<puddles.size();i++){
-        count[puddles[i][1] - 1][puddles[i][0] - 1]=-1;
-    }
-
-
-    for (int i = 0; i < n; ++i) {
-        count[i][0] = 1;
-    }
-    for (int i = 0; i < m; ++i) {
-        count[0][i] = 1;
-    }
-
-    for(int i=1;i<n;i++){
-        for(int j=1;j<m;j++){
-            if(count[i][j]==-1){
-                continue;
-            }
-
-            if(count[i][j-1] == -1){
-                count[i][j] = count[i-1][j];
-            }
-            else if(count[i-1][j] == -1){
-                count[i][j] = count[i][j-1];
-            }
-            else{
-                count[i][j]=(count[i-1][j]+count[i][j-1])%1000000007;
-            }
-        }
-    }
-
-    answer = count[n-1][m-1];
-
-    return answer;
-}*/
