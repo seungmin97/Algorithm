d5e0665268fb7ff4a6ee9da14265a8f2626c2cf3
seungmin97 <lsm0341@naver.com>
2020-02-04 13:57:06 +0900

    2020-02-03~2020-02-04

--- Programmers/Level3/visit_distance.cpp
+++ Programmers/Level3/visit_distance.cpp
@@ -11,41 +11,45 @@ using namespace std;
 int solution(string dirs) {
     int answer = 0;
 
-    int arr1[11][10];
-    int arr2[11][10];
+    //왼오
+    int arr1[11][11];
+    //위아래
+    int arr2[11][11];
+
     for (int i = 0; i < 11; i++) {
-        memset(arr1[i], 0, sizeof(int) * 10);
-        memset(arr2[i], 0, sizeof(int) * 10);
+        memset(arr1[i], 0, sizeof(int) * 11);
+        memset(arr2[i], 0, sizeof(int) * 11);
     }
 
-    int x = 0, y = 0;
-    int check = 0;
+    int x = 5, y = 5;
     for (int i = 0; i < dirs.length(); ++i) {
-        if(dirs[i] == 'U' && y < 5){
-            if(arr1[x][y + 2] == 0){
+
+        if(dirs[i] == 'U' && y < 10){
+            if(arr2[x][y] == 0){
                answer++;
-               arr1[x][y+2] = 1;
+               arr2[x][y] = 1;
             }
             y++;
         }
-        else if(dirs[i] == 'D' && y > -5){
-            if(arr1[x][y+1] == 0){
+        else if(dirs[i] == 'D' && y > 0){
+            if(arr2[x][y-1] == 0){
                 answer++;
-                arr1[x][y+1] = 1;
+                arr2[x][y-1] = 1;
             }
+
             y--;
         }
-        else if(dirs[i] == 'L' && x > -5){
-            if(arr2[x+1][y] == 0){
+        else if(dirs[i] == 'L' && x > 0){
+            if(arr1[x-1][y] == 0){
                 answer++;
-                arr2[x+1][y] = 1;
+                arr1[x-1][y] = 1;
             }
             x--;
         }
-        else if(dirs[i] == 'R' && x < 5){
-            if(arr2[x+2][y] == 0){
+        else if(dirs[i] == 'R' && x < 10){
+            if(arr1[x][y] == 0){
                 answer++;
-                arr2[x+2][y] = 1;
+                arr1[x][y] = 1;
             }
             x++;
         }
@@ -60,9 +64,14 @@ int solution(string dirs) {
 
 int main(){
 
-    cout << solution("LR") << endl;
-    cout << solution("ULURRDLLU") << endl;
-    cout << solution("LULLLLLLU") << endl;
+    cout << solution("LR") << endl; //1
+    cout << solution("ULURRDLLU") << endl; //7
+    cout << solution("LULLLLLLU") << endl; //7
+    cout <<  solution("LLLLRLRLRLL") << endl; //5
+    cout << solution("UUUUDUDUDUUU") << endl; //5
+    cout << solution("LURDLURDLURDLURDRULD") << endl; //7
+    cout << solution("RRRRRRRRUUUUUUULU") << endl; //11
+
 
     return 0;
 }
